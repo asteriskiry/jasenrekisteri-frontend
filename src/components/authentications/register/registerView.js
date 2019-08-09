dc79e9e03f0f9e4ed9a17a399eb09744c8144443
mjturt <maks.turtiainen@gmail.com>
2019-08-10 00:36:29 +0300

    Frontend form validations: 'join' -view

--- src/components/authentications/register/registerView.js
+++ src/components/authentications/register/registerView.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    ValidationForm,
+    TextInput,
+    SelectGroup,
+} from 'react-bootstrap4-form-validation';
+import validator from 'validator';
+import './registerView.css';
 
 import { Alert, Form, Button, Row, Col } from 'react-bootstrap';
 
@@ -19,20 +26,31 @@ const RegisterView = ({
     <MainComponent big="true">
         <h3 className="text-center">Liity jäseneksi</h3>
         <p>
-            Voit jättää jäsenhakemuksesi tästä. Täytön jälkeen siirryt
-            valitsemaan maksutapaa. Onnistuneen maksun jälkeen pääset
+            Voit jättää jäsenhakemuksesi Asteriski ry:lle tästä. Täytön jälkeen
+            siirryt valitsemaan maksutapaa. Onnistuneen maksun jälkeen pääset
             kirjautumaan sisään sähköpostiisi lähetetyllä salasanalla. Tämän
             jälkeen hallitus hyväksyy jäsenyytesi mahdollisimman pian. Mikäli
             jäsenyyttäsi ei hyväksytä, jäsenmaksu palautetaan.
         </p>
-        <Form onSubmit={handleRegistration}>
+        <ValidationForm
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleRegistration(e);
+            }}
+        >
             <Row>
                 <Col md>
                     <Form.Group>
                         <Form.Label>Etunimi</Form.Label>
-                        <Form.Control
-                            type="text"
+                        <TextInput
                             name="firstName"
+                            id="firstName"
+                            errorMessage={{
+                                required: 'Etunimi on pakollinen.',
+                                pattern: 'Tarkista etunimi.',
+                            }}
+                            required
+                            pattern="[A-ZÅÄÖa-zåäö]+"
                             onChange={handleInputChange}
                         />
                     </Form.Group>
@@ -40,9 +58,15 @@ const RegisterView = ({
                 <Col md>
                     <Form.Group>
                         <Form.Label>Sukunimi</Form.Label>
-                        <Form.Control
-                            type="text"
+                        <TextInput
                             name="lastName"
+                            id="lastName"
+                            errorMessage={{
+                                required: 'Sukunimi on pakollinen.',
+                                pattern: 'Tarkista sukunimi.',
+                            }}
+                            required
+                            pattern="[A-ZÅÄÖa-zåäö]+"
                             onChange={handleInputChange}
                         />
                     </Form.Group>
@@ -52,9 +76,15 @@ const RegisterView = ({
                 <Col md>
                     <Form.Group>
                         <Form.Label>UTU-tunnus (ilman @utu.fi)</Form.Label>
-                        <Form.Control
-                            type="text"
+                        <TextInput
                             name="utuAccount"
+                            id="utuAccount"
+                            errorMessage={{
+                                required: 'UTU-tunnus on pakollinen.',
+                                pattern: 'Tarkista UTU-tunnus.',
+                            }}
+                            required
+                            pattern="[A-ZÅÄÖa-zåäö]+"
                             onChange={handleInputChange}
                         />
                     </Form.Group>
@@ -62,9 +92,14 @@ const RegisterView = ({
                 <Col md>
                     <Form.Group>
                         <Form.Label>Sähköposti</Form.Label>
-                        <Form.Control
-                            type="text"
+                        <TextInput
                             name="email"
+                            id="email"
+                            errorMessage={{
+                                required: 'Sähköpostiosoite on pakollinen.',
+                                validator: 'Tarkista sähköpostiosoite.',
+                            }}
+                            validator={validator.isEmail}
                             onChange={handleInputChange}
                         />
                     </Form.Group>
@@ -72,33 +107,41 @@ const RegisterView = ({
             </Row>
             <Row>
                 <Col md>
-            <Form.Group>
-                <Form.Label>Kotikunta</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="hometown"
-                    onChange={handleInputChange}
-                />
-            </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Kotikunta</Form.Label>
+                        <TextInput
+                            name="hometown"
+                            id="hometown"
+                            errorMessage={{
+                                required: 'Kotikunta on pakollinen.',
+                                pattern: 'Tarkista kotikunta.',
+                            }}
+                            required
+                            pattern="[A-ZÅÄÖa-zåäö]+"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
                 </Col>
                 <Col md>
-            <Form.Group>
-                <Form.Label>Jäsenmaksu</Form.Label>
-                <Form.Control
-                    as="select"
-                    name="productId"
-                    value={productId}
-                    onChange={handleInputChange}
-                >
-                    <option value="1111">1 vuosi - 5€</option>
-                    <option value="1555">5 vuotta - 20€</option>
-                    {currentDate.getMonth() > 6 ? (
-                        <option value="1222">
-                            Pilttitarjous: 1,5 vuotta - 7€
-                        </option>
-                    ) : null}
-                </Form.Control>
-            </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Jäsenmaksu</Form.Label>
+                        <SelectGroup
+                            name="productId"
+                            id="productId"
+                            required
+                            errorMessage="Valitse jäsenyys."
+                            value={productId}
+                            onChange={handleInputChange}
+                        >
+                            <option value="1111">1 vuosi - 5€</option>
+                            <option value="1555">5 vuotta - 20€</option>
+                            {currentDate.getMonth() > 6 ? (
+                                <option value="1222">
+                                    Pilttitarjous: 1,5 vuotta - 7€
+                                </option>
+                            ) : null}
+                        </SelectGroup>
+                    </Form.Group>
                 </Col>
             </Row>
             {productId === '1222' ? (
@@ -134,7 +177,7 @@ const RegisterView = ({
             <Button type="submit" variant="success">
                 <FontAwesomeIcon icon="sign-in-alt" /> Siirry maksamaan
             </Button>
-        </Form>
+        </ValidationForm>
         <hr />
         <div className="btm-links">
             <p>
